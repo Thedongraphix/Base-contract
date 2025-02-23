1a8fec50399d37b9d6e6acb70f11b2aa3b1ebf72
Thedongraphix <chrisoketchcn@gmail.com>
2025-02-23 15:45:43 +0300

    New files without workflow

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,40 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    address public owner;
+    
+    event NumberSet(uint256 oldValue, uint256 newValue);
+    event NumberIncremented(uint256 newValue);
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
 
-    function setNumber(uint256 newNumber) public {
+    function setNumber(uint256 newNumber) public onlyOwner {
+        uint256 oldValue = number;
         number = newNumber;
+        emit NumberSet(oldValue, newNumber);
     }
 
     function increment() public {
         number++;
+        emit NumberIncremented(number);
+    }
+    
+    function decrement() public {
+        require(number > 0, "Cannot decrement below zero");
+        number--;
+        emit NumberIncremented(number);
+    }
+    
+    function getNumber() public view returns (uint256) {
+        return number;
     }
 }
